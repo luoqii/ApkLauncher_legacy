bd8d0a29ba0473b356cfc7d3d27839f32893177a
bysong <bysong@tudou.com>
2015-02-07 14:04:27 +0800

    add log

--- src/org/bbs/apklauncher/emb/Host_Service.java
+++ src/org/bbs/apklauncher/emb/Host_Service.java
@@ -9,9 +9,14 @@ import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.content.res.Configuration;
 import android.os.IBinder;
+import android.util.Log;
 
 @SuppressLint("NewApi")
 public abstract class Host_Service extends Service {
+	private static final String TAG = Host_Service.class.getSimpleName();
+	private static boolean DEBUG_LIEFT_CYCLE = true;
+	private static boolean DEBUG_MEMORY = true;
+	private static boolean DEBUG = true;
 	
 	Target_Service mTargetService;
 	protected LazyContext mTargetContext;
@@ -34,16 +39,18 @@ public abstract class Host_Service extends Service {
 	
 	@Override
 	public IBinder onBind(Intent intent) {
-		onPrepareServiceStub(intent);
-		if (null != mTargetService) {
-			return mTargetService.onBind(intent);
-		} else {
-			return null;
+		if (DEBUG) {
+			Log.d(TAG, "onBind(). intent: " + intent);
 		}
+		onPrepareServiceStub(intent);
+		return mTargetService.onBind(intent);
 	}
 
 	@Override
 	public void onCreate() {
+		if (DEBUG_LIEFT_CYCLE) {
+			Log.d(TAG, "onCreate(). ");
+		}
 		super.onCreate();
 		if (null != mTargetService) {
 			mTargetService.onCreate();
@@ -69,6 +76,9 @@ public abstract class Host_Service extends Service {
 	
 	@Override
 	public void onDestroy() {
+		if (DEBUG_LIEFT_CYCLE) {
+			Log.d(TAG, "onDestroy(). ");
+		}
 		super.onDestroy();
 		if (null != mTargetService) {
 			mTargetService.onDestroy();
@@ -77,6 +87,9 @@ public abstract class Host_Service extends Service {
 	
 	@Override
 	public void onConfigurationChanged(Configuration newConfig) {
+		if (DEBUG) {
+			Log.d(TAG, "onConfigurationChanged(). newConfig: " + newConfig);
+		}
 		super.onConfigurationChanged(newConfig);
 		if (null != mTargetService) {
 			mTargetService.onConfigurationChanged(newConfig);
@@ -85,6 +98,9 @@ public abstract class Host_Service extends Service {
 	
 	@Override
 	public void onLowMemory() {
+		if (DEBUG_MEMORY) {
+			Log.d(TAG, "onLowMemory(). ");
+		}
 		super.onLowMemory();
 		if (null != mTargetService) {
 			mTargetService.onLowMemory();
@@ -93,6 +109,9 @@ public abstract class Host_Service extends Service {
 	
 	@Override
 	public void onTrimMemory(int level) {
+		if (DEBUG_MEMORY) {
+			Log.d(TAG, "onTrimMemory(). level: " + Util.toMemoryLevel(level));
+		}
 		super.onTrimMemory(level);
 		if (null != mTargetService) {
 			mTargetService.onTrimMemory(level);
---
+++ src/org/bbs/apklauncher/emb/Util.java
@@ -0,0 +1,10 @@
+package org.bbs.apklauncher.emb;
+
+public class Util {
+	public static final String toMemoryLevel(int level) {
+		String levelStr = "";
+		levelStr = level + "";
+		
+		return levelStr;
+	}
+}
